fe1c6ecb9b197af2247ced9ac8e0e3166eb1c2a3
detayaa <drugochenko.taya@mail.ru>
2022-08-29 19:39:08 +0700

    1 & 2

--- src/main.java
+++ src/main.java
@@ -1,5 +1,26 @@
 public class main {
     public static void main(String[] args) {
 
+        // задание 1
+        int age = 16;
+        if (age >= 18) {
+            System.out.println("Поздравляем");
+        }
+        if (age < 18) {
+            System.out.println("Возраст ещё не наступил");
+        }
+
+        // задание 2
+        int ageChild = 16;
+        if (ageChild >= 7 && ageChild < 18) {
+            System.out.println("Ребенок ходит в школу");
+        }
+        if (ageChild >= 18 && ageChild < 24) {
+            System.out.println("Человек может отправлять в университет");
+        }
+        if (ageChild >= 24) {
+            System.out.println("Пора искать работу");
+        }
+
     }
 }
